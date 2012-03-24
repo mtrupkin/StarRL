18b9e9bde3343755b13f3151d1c5edb39e4a386b
Mike Trupkin <mike.trupkin@gmail.com>
2012-03-23 23:50:44 -0700

    added ship detail widget

---
+++ StarRL/View/EntityView/ShipDetailComposite.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ConsoleLib;
+using ConsoleLib.Widget;
+using Flagship;
+using StarRL.Widget;
+
+namespace StarRL
+{
+	public class ShipDetailComposite : Composite
+	{
+		public Ship Ship {get;set;}
+        EntityDetailComposite EntityWidget {get;set;}
+		TextWidget ShieldsWidget { get; set; }
+		TextWidget TorpedoesWidget { get; set; }
+		TextWidget PhasersWidget { get; set; }
+		TextWidget ScannersWidget { get; set; }
+
+		public ShipDetailComposite (String title) 
+		{
+			EntityWidget = new EntityDetailComposite(title);
+			
+			Width = EntityWidget.Width;
+			Height = EntityWidget.Height + 4;
+			
+            ShieldsWidget = new TextWidget() { Width = this.Width };
+            TorpedoesWidget = new TextWidget() { Width = this.Width };
+            PhasersWidget = new TextWidget() { Width = this.Width };
+            ScannersWidget = new TextWidget() { Width = this.Width };
+		
+			LayoutManager layoutManager = new LayoutManager ();
+			layoutManager.AddControl(EntityWidget);
+            layoutManager.AddControl(ShieldsWidget);
+			layoutManager.AddControl(TorpedoesWidget);
+			layoutManager.AddControl(PhasersWidget);
+			layoutManager.AddControl(ScannersWidget);
+			
+			AddLayoutManager(layoutManager);
+		}
+
+      
+		public override void Render ()
+		{
+			//Con.Clear();
+								
+			base.Render();
+		}
+		
+        public void SetTitle(String title)
+        {
+            EntityWidget.SetTitle(title);
+        }
+
+        public void SetShip(Ship ship)
+		{			
+			EntityWidget.SetEntity(ship);
+
+            Ship = ship;
+        }
+	}
+}
