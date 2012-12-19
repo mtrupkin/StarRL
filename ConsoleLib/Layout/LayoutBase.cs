587605ca9197eeeb37729fa6b41f6832a21f24ad
mtrupkin <mike.trupkin@gmail.com>
2012-12-18 16:45:04 -0800

    rename base control classes

---
+++ ConsoleLib/Layout/LayoutBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleLib
+{
+    public abstract class LayoutBase : Layout
+    {
+        public int MinWidth { get; protected set; }
+        public int MinHeight { get; protected set; }
+
+        public int Width { get; protected set; }
+        public int Height { get; protected set; }
+
+        public abstract void LayoutControls(List<Control> controls);
+
+        public void SetMinSize(int width, int height)
+        {
+            MinWidth = width;
+            MinHeight = height;
+        }
+    }
+}
