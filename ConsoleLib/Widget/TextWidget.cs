ab02650a5471c1052494e4d9dae45c95870af980
mtrupkin <mike.trupkin@gmail.com>
2012-12-18 16:43:28 -0800

    preserve minimum length for text widgets

--- ConsoleLib/Widget/TextWidget.cs
+++ ConsoleLib/Widget/TextWidget.cs
@@ -4,24 +4,32 @@ namespace ConsoleLib
 {
 	public class TextWidget : ControlBase
 	{
+        protected int MinWidth { get; set; }
+
         public String TextValue { get; protected set; }
 
         public TextWidget(Composite parent, int width)
             : base(parent, width, 1)
         {
-            
+            MinWidth = Width;
         }
 
         public TextWidget(Composite parent, String text)
             : base(parent, text.Length, 1)
 		{
+            MinWidth = Width;
             TextValue = text;
 		}
 
         public void SetText(String text)
         {
             TextValue = text;
-            Resize(text.Length, 1);
+            int newSize = text.Length;
+            if (newSize > MinWidth)
+            {
+                MinWidth = newSize;
+                Resize(MinWidth, 1);
+            }
         }
 		
 		public override void Render ()
